4b7d9004fbc6c7ca69db5d48c96e7ee4c1eb796f
Anais Milagros <93013604+ANAISBT@users.noreply.github.com>
2024-09-23 23:22:32 -0500

    Fronted Login

---
+++ app/src/main/java/com/example/pillpop_doctor/LoginView.kt
@@ -0,0 +1,12 @@
+package com.example.pillpop_doctor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
