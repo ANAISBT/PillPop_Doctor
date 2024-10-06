ae05679cd93a4d66c5a25f232f9fc848ebfb0e3c
Anais Milagros <anais.bustamante@paku.pe>
2024-10-06 00:34:43 -0500

    cambiarContraseña y AcercaPillPop

---
+++ app/src/main/java/com/example/pillpop_doctor/CambiarComtraseñaView.kt
@@ -0,0 +1,12 @@
+package com.example.pillpop_doctor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CambiarComtraseñaView  : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cambiar_contrasena_view)
+
+    }
+}
